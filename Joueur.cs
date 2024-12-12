9e3a1dc13ed66bf564221d21a9ef336a238a47c3
Arixe05 <183124953+Arixe05@users.noreply.github.com>
2024-12-12 09:45:50 +0100

    petite modif classe joueur, comptage des pts

--- Joueur.cs
+++ Joueur.cs
@@ -5,25 +5,43 @@
     public string Pseudo { get; set; } //pseudo 
     public int Score { get; private set; } // Le score d
     public List<string> MotsTrouves { get; private set; } //iste des mots trouvé par le joueur
+    private (string lettre, int score, int nombre)[] LettreScore;
 
     // Constructeur
-    public Joueur(string pseudo)
+    public Joueur(string pseudo, (string lettre, int score, int nombre)[] infoLettre)
     {
         Pseudo = pseudo;
         Score = 0;
         MotsTrouves = new List<string>();
+        LettreScore = infoLettre;
     }
     /// ajCoute un mot trouvé par le joueur, en vérifiant qu'il n'est pas déjà dans la liste.
     /// True si le mot est ajouté, False s'il est déjà présent
     public bool AjouterMot(string mot, int points)
     {
+        bool testmot = false;
         if (!MotsTrouves.Contains(mot))
         {
             MotsTrouves.Add(mot);
-            Score += points;
-            return true;
+            int point = 0; 
+            string m = mot;
+            while (m != "")
+            {
+                for (int i = 0; i<LettreScore.Length; i++ )
+                {
+                    if (m[0].ToString() == LettreScore[i].lettre)
+                    {
+                        point += LettreScore[i].score;
+                        m = m.Substring(1);
+                    }
+                }
+            }
+            Console.WriteLine("Le mot "+ mot.ToUpper()+" vaut "+ point + " points");
+            testmot = true;
+
+            
         }
-        return false;
+        return testmot;
     }
 
     /// Réinitialiser les mots trouvés pour un nouveau tour.
