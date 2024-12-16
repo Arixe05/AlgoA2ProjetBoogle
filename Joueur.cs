da23dba8ada9008f86e86f2baaa64bb9d1797a90
Arixe05 <183124953+Arixe05@users.noreply.github.com>
2024-12-16 16:13:53 +0100

    petite modif classe ainsi que Program complet+timer

--- Joueur.cs
+++ Joueur.cs
@@ -5,6 +5,8 @@
     public string Pseudo { get; set; } //pseudo 
     public int Score { get; private set; } // Le score d
     public List<string> MotsTrouves { get; private set; } //iste des mots trouvé par le joueur
+
+    public int NBmot;
     private (string lettre, int score, int nombre)[] LettreScore;
 
     // Constructeur
@@ -14,34 +16,42 @@
         Score = 0;
         MotsTrouves = new List<string>();
         LettreScore = infoLettre;
+        NBmot = 0;
     }
     /// ajCoute un mot trouvé par le joueur, en vérifiant qu'il n'est pas déjà dans la liste.
     /// True si le mot est ajouté, False s'il est déjà présent
-    public bool AjouterMot(string mot, int points)
+    public void AjouterMot(string mot)
     {
-        bool testmot = false;
+        
         if (!MotsTrouves.Contains(mot))
         {
             MotsTrouves.Add(mot);
             int point = 0; 
-            string m = mot;
-            while (m != "")
+            string m = mot.ToUpper();
+           
+            while (m!="")
             {
-                for (int i = 0; i<LettreScore.Length; i++ )
+             
+                for (int k = 0; k<LettreScore.Length&&m!=""; k++ )
                 {
-                    if (m[0].ToString() == LettreScore[i].lettre)
+                    
+                    if (m[0] == Convert.ToChar(LettreScore[k].lettre))
                     {
-                        point += LettreScore[i].score;
+                        point += LettreScore[k].score;
                         m = m.Substring(1);
+                        
+                        
                     }
                 }
             }
-            Console.WriteLine("Le mot "+ mot.ToUpper()+" vaut "+ point + " points");
-            testmot = true;
-
-            
+            NBmot++;
+            Score += point;
+            Console.WriteLine("Le mot "+ mot.ToUpper()+" vaut "+ point + " points");   
         }
-        return testmot;
+        else {
+            Console.WriteLine("le mot a déjà été entré ou n'a pas pu etre ajouté");
+        }
+        
     }
 
     /// Réinitialiser les mots trouvés pour un nouveau tour.
@@ -55,9 +65,13 @@
     {
         Console.WriteLine($"Pseudo: {Pseudo}");
         Console.WriteLine($"Score: {Score}");
-        Console.WriteLine($"Mots trouvés: {string.Join(", ", MotsTrouves)}");
+        Console.WriteLine($"Mots trouvés: {NBmot}");
     }
-    // cette ligne est un test
+    
+    public int score()
+    {
+        return Score;
+    }
     
 }
     
--- Program.cs
+++ Program.cs
@@ -1,14 +1,194 @@
 ﻿using System.Linq.Expressions;
 using System.IO;
+using System.Timers;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography;
+
 {
+
+
+// initialisation du dico
+Console.WriteLine("Initialisation du jeux...\nVeuillez entrez la langue dans laquelle vous voulez jouer (FR ou EN).");
+Dictionaire dico = new Dictionaire(Console.ReadLine());
+dico.toString();
+
+// intitialisation de la liste des lettres pour les dés 
+        // création du tableau de lettre utilisé pour la création des dés, à metre dans le main lors de la création du main
+    (string lettre, int score, int nombre)[] tabde = new (string, int, int)[26];
+    string contenu = File.ReadAllText("Lettres.txt");
+    for (int i = 0; i<26; i++)
+    {
+        tabde[i].lettre = Convert.ToString(contenu.Substring(0, contenu.IndexOf(";")));
+        contenu = contenu.Substring(contenu.IndexOf(";")+1);
+        tabde[i].score = Convert.ToInt32(contenu.Substring(0, contenu.IndexOf(";")));
+        contenu = contenu.Substring(contenu.IndexOf(";")+1);
+        tabde[i].nombre = Convert.ToInt32(contenu.Substring(0, contenu.IndexOf(";")));
+        contenu = contenu.Substring(contenu.IndexOf(";")+1);
+        
+    }
+    // test pour voir si notre tableau avec les différente valeur est correcte
+    for(int i = 0; i< tabde.Length; i++)
+    {
+        Console.WriteLine($"{tabde[i].lettre}, {tabde[i].score}, {tabde[i].nombre}");
+    }
+    //création d'une liste avec des lettres pondéré
+    List<string> ListeLettrePondéré = new List<string>();
+     for (int i = 0; i<26; i++)
+     {
+        for (int j =tabde[i].nombre; j>0; j--)
+        {
+            ListeLettrePondéré.Add(tabde[i].lettre);
+        }
+     } 
+
+     // création du tableau de jeux
+     int hauteur = -1;
+     int largeur =-1;      
+     while (hauteur<=1)
+    {
+        Console.WriteLine("veuillez entrer une hauteur>1");
+        try{
+        hauteur = Convert.ToInt32(Console.ReadLine());
+        }
+        catch{
+            Console.WriteLine("ce n'est pas le bon format") ;
+        }
+    }
+    while (largeur<=1)
+    {
+        Console.WriteLine("veuillez entrer une largeur (>1)");
+        try{
+            largeur = Convert.ToInt32(Console.ReadLine());
+        }
+        catch{
+            Console.WriteLine("ce n'est pas le bon format") ;
+        }
+    }
+    Plateau p = new Plateau(hauteur, largeur, ListeLettrePondéré);
+   
+
+    // créatrion des Joueurs
+
+    Console.WriteLine("veuillez entre le pseudo du 1er joueur");
+    Joueur j1 = new Joueur(Console.ReadLine(), tabde);
+    Console.WriteLine("veuillez entre le pseudo du 2nd joueur");
+    Joueur j2 = new Joueur(Console.ReadLine(), tabde);
+
+    // Nombre de lanche du jeux
+
+    int tour = -1;
+    Console.WriteLine("Veuillez entrer le nombre de tour (>=1)");
+    while (tour <1)
+    {
+        
+        try{
+            
+            tour = Convert.ToInt32(Console.ReadLine());
+        }
+        catch{
+            Console.WriteLine("veuillez entrer une nouvelle valeur >=1");
+        }
+    }
+    // début de la partie
+    tour = tour*2;
+    // création du timer
+    Tim T = new Tim(20*1000); // le 60 correspond à la durré du tour d'un joueur
     
-De d1 = new De();
-Console.WriteLine("jsp gros clc");
 
-    
+    string m = ""; //mot trouvé
+    // boucle du jeux, 
+    T.lancetime();
+    while (tour !=0)
+    {   
+        Console.WriteLine($"\nc'est au joueur {tour%2+1}  de jouer\n");
+        T.start();
+        if(tour%2 == 0)
+        {
+            j1.AfficherInfos();
+            p.NouvPlat();
+            Console.WriteLine(p.AfficherPlat());
+         
+            while (T.TFT()==false)
+            {
+                m = Console.ReadLine();
+                if (dico.RechDico(m) == true && p.Test_Plateau(m) == true)
+                {
+                    j1.AjouterMot(m);
+                }
+                else if (dico.RechDico(m)== false)
+                {
+                    Console.WriteLine("le mot n'est pas dans le dictionaire");
+                }
+                else {Console.WriteLine("le mot n'est pas dans le plateau");}
+                Console.WriteLine("Vous avez un score de "+j1.score());
+                
+                
+            }
+            j1.ReinitialiserMots();
+           
+      
+        }
+        else
+        {
+            j2.AfficherInfos();
+            p.NouvPlat();
+             Console.WriteLine(p.AfficherPlat());
+          
+            while (T.TFT()==false)
+            {
+                m = Console.ReadLine();
+                if (dico.RechDico(m)== true && p.Test_Plateau(m)== true)
+                {
+                    j2.AjouterMot(m);
+                }
+                else if (dico.RechDico(m)== false)
+                {
+                    Console.WriteLine("le mot n'est pas dans le dictionaire");
+                }
+                else {Console.WriteLine("le mot n'est pas dans le plateau");}
+                Console.WriteLine("Vous avez un score de "+j2.score());
+                
+                
+            }
+           j2.ReinitialiserMots();
+      
+        }
+         T.stop();
+        T.TFF();
+        tour--;
+    } 
+
+     // annonce de la fin et des score
+
+    Console.WriteLine("\n\nVoici les scores de la partie\n");
+    j1.AfficherInfos();
+    Console.WriteLine();
+    j2.AfficherInfos();
+    Console.WriteLine();
+    if(j1.score()>j2.score())
+    {
+        Console.WriteLine("C'est le joueur 1 qui à gagné");
+    }
+    else if (j1.score()==j2.score())
+
+    {
+         Console.WriteLine("Il y a égalité");
+    }
+    else{ Console.WriteLine("C'est le joueur 2 qui a gagné");}
+
+
+
+ }
 
 
 
 
 
-}
+
+
+             
+
+
+
+
+
